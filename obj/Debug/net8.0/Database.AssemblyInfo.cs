6f8e6e830baa674a0ea6b18662ef6d32c87141c5
Karumi123 <forg3335@gmail.com>
2026-07-27 16:37:24 +0700

    changed database name

--- obj/Debug/net8.0/Database.AssemblyInfo.cs
+++ obj/Debug/net8.0/Database.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Database")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79045dba5b7a548b2a837786a3803d8e44165474")]
 [assembly: System.Reflection.AssemblyProductAttribute("Database")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Database")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
